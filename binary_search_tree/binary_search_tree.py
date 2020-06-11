36a23df057efbaa861a58c484a7e461889e63f1f
ph5500 <philfives@gmail.com>
2020-06-11 17:04:12 -0600

    reach bst day 2 mvp tests all passing now

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,79 +9,63 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from linked_list import LinkedList
+from bst_doubly_linked_list import DoublyLinkedList
 from collections import deque
-# from linked_list import LinkedList
-# from bst_doubly_linked_list import DoublyLinkedList
-# class Stack:
-#     def __init__(self):
-#         self.size = 0
-#         # self.storage = []
-#         self.storage = LinkedList()
 
-#     def __len__(self):
-#         # return len(self.storage)
-#         return self.size
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # self.storage = []
+        self.storage = LinkedList()
 
-#     def push(self, value):
-#         # return self.storage.append(value)
-#         self.storage.add_to_end(value)
-#         self.size += 1
+    def __len__(self):
+        # return len(self.storage)
+        return self.size
 
-#     def pop(self):
-#         # if len(self.storage) == 0:
-#         #     return None
-#         # else:
-#         #     return self.storage.pop()
+    def push(self, value):
+        # return self.storage.append(value)
+        self.storage.add_to_tail(value)
+        self.size += 1
 
-#         if self.storage.head:
-#             self.size -= 1
-#             return self.storage.remove_from_end()
+    def pop(self):
+        # if len(self.storage) == 0:
+        #     return None
+        # else:
+        #     return self.storage.pop()
+
+        if self.storage.head:
+            self.size -= 1
+            return self.storage.remove_tail()
 
 
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         # self.storage = []
-#         # self.storage = LinkedList()
-#         self.storage = DoublyLinkedList()
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # self.storage = []
+        # self.storage = LinkedList()
+        self.storage = DoublyLinkedList()
 
-#     def __len__(self):
-#         # return len(self.storage)
-#         return self.size
+    def __len__(self):
+        # return len(self.storage)
+        return self.size
 
-#     def enqueue(self, value):
-#         # self.storage.append(value)
-#         print(value)
-#         self.size += 1
-#         self.storage.add_to_tail(value)
+    def enqueue(self, value):
+        # self.storage.append(value)
+        print(value)
+        self.size += 1
+        self.storage.add_to_tail(value)
         
 
-#     def dequeue(self):
-#         # if len(self.storage) == 0:
-#         #     return None
-#         # else:
-#         #     return self.storage.pop(0)
-#         if self.storage.head:
-#             self.size -= 1
-#             return self.storage.remove_from_head()
-#         return None
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def dequeue(self):
+        # if len(self.storage) == 0:
+        #     return None
+        # else:
+        #     return self.storage.pop(0)
+        if self.storage.head:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        return None
 
 class BSTNode:
     def __init__(self, value):
@@ -207,7 +191,7 @@ class BSTNode:
             if node.left is not None:
                 node.in_order_print(node.left)
                         
-                print(node.value)
+            print(node.value)
                     
             if node.right is not None:
                 node.in_order_print(node.right)
@@ -227,11 +211,11 @@ class BSTNode:
                 current = queue.popleft()
                 print(current.value)
             # if there is a node to the left of the current node, add to queue
-            if current.left is not None:
-                queue.append(current.left)
+                if current.left is not None:
+                    queue.append(current.left)
             # if there is a node to the right of the current node, add to the queue
-            if current.right is not None:
-                queue.append(current.right)
+                if current.right is not None:
+                    queue.append(current.right)
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
