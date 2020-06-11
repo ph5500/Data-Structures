980f8e4bb2144bf8cc4262eeb736dedf88cca2ae
ph5500 <philfives@gmail.com>
2020-06-11 15:45:23 -0600

    bst day2 mvp i believe

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,8 +9,80 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
-# from collections import queue
-# import linked_list from LinkedList
+from collections import deque
+# from linked_list import LinkedList
+# from bst_doubly_linked_list import DoublyLinkedList
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         # self.storage = []
+#         self.storage = LinkedList()
+
+#     def __len__(self):
+#         # return len(self.storage)
+#         return self.size
+
+#     def push(self, value):
+#         # return self.storage.append(value)
+#         self.storage.add_to_end(value)
+#         self.size += 1
+
+#     def pop(self):
+#         # if len(self.storage) == 0:
+#         #     return None
+#         # else:
+#         #     return self.storage.pop()
+
+#         if self.storage.head:
+#             self.size -= 1
+#             return self.storage.remove_from_end()
+
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         # self.storage = []
+#         # self.storage = LinkedList()
+#         self.storage = DoublyLinkedList()
+
+#     def __len__(self):
+#         # return len(self.storage)
+#         return self.size
+
+#     def enqueue(self, value):
+#         # self.storage.append(value)
+#         print(value)
+#         self.size += 1
+#         self.storage.add_to_tail(value)
+        
+
+#     def dequeue(self):
+#         # if len(self.storage) == 0:
+#         #     return None
+#         # else:
+#         #     return self.storage.pop(0)
+#         if self.storage.head:
+#             self.size -= 1
+#             return self.storage.remove_from_head()
+#         return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -79,82 +151,107 @@ class BSTNode:
         # if yes, call it's for_each with the same fn            
             self.right.for_each(fn)
 
-
-#  			def depth_first_for_each(self, fn):
-# 				# this method specifically does want to traverse every tree node 
-# 				# this has to call the fn on self.value 
-# 				fn(self.value)
-# ​
-# 				# how do we propagate to all the other nodes in the tree? 
-# 				# is there a left child? 
-# 				if self.left:
-# 					# if yes, then call its `for_each` with the same fn 
-# 					self.left.depth_first_for_each(fn)
-# 					# is there a right child?
-# 					if self.right:
-# 						# if yes, then call its `for_each` with the same fn 
-# 						self.right.depth_first_for_each(fn)
-# ​
-# 						def iter_depth_first_for_each(self, fn):
-# 							# with depth-first traversal, there's a certain order to when we visit nodes 
-# 							# what's that order? 
-# 							# init a stack to keep track of the order of nodes we visited 
-# 							stack = []
-# 							# add the first node to our stack 
-# 							stack.append(self)
-# 							# continue traversing until our stack is empty
-# 							while len(stack) > 0: 
-# 								# pop off the stack 
-# 								current_node = stack.pop()
-# 								# add its children to the stack 
-# 								# add the right child first and left child second
-# 								# to ensure that left is popped off the stack first 
-# 								if current_node.right:
-# 									stack.append(current_node.right)
-# 									if current_node.left:
-# 										stack.append(current_node.left)
-# 										# call the fn function on self.value 
-# 										fn(self.value)
-# ​
-# 										def iter_breadth_first_search(self, fn):
-# 											# breadth first traversal follows FIFO ordering of its nodes
-# 											# init a deque 
-# 											q = deque()
-# 											# add the first node to our q 
-# 											q.append(self)
-# ​
-# 											while len(q) > 0:
-# 												current_node = q.popleft()
-# 												if current_node.left:
-# 													q.append(current_node.left)
-# 													if current_node.right:
-# 														q.append(current_node.right)
-# 														fn(self.value)
+# def depth_first_for_each(self, fn):
+#         # this method specifically does want to traverse every tree node
+#         # this has to call the fn on self.value
+#         fn(self.value)
+#         # how do we propagate to all the other nodes in the tree?
+#         # is there a left child?
+#         if self.left:
+#             # if yes, then call its for_each with the same fn
+#             self.left.depth_first_for_each(fn)
+#             # is there a right child?
+#             if self.right:
+#                 # if yes, then call its for_each with the same fn
+#                 self.right.depth_first_for_each(fn)
+# def iter_depth_first_for_each(self, fn):
+#         # with depth-first traversal, there's a certain order to when we visit nodes
+#         # what's that order?
+#         # init a stack to keep track of the order of nodes we visited
+#         stack = []
+#         # add the first node to our stack
+#         stack.append(self)
+#         # continue traversing until our stack is empty
+#         while len(stack) > 0:
+#             # pop off the stack
+#             current_node = stack.pop()
+#             # add its children to the stack
+#             # add the right child first and left child second
+#             # to ensure that left is popped off the stack first
+#             if current_node.right:
+#                 stack.append(current_node.right)
+#                 if current_node.left:
+#                     stack.append(current_node.left)
+#                     # call the fn function on self.value
+#                     fn(self.value)
+# def iter_breadth_first_search(self, fn):
+#         # breadth first traversal follows FIFO ordering of its nodes
+#         # init a deque
+#         q = deque()
+#         # add the first node to our q
+#         q.append(self)
+#         while len(q) > 0:
+#             current_node = q.popleft()
+#             if current_node.left:
+#                 q.append(current_node.left)
+#                 if current_node.right:
+#                     q.append(current_node.right)
+#                     fn(self.value)
+                    
             #same as dequeing from our stack 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+            if node.left is not None:
+                node.in_order_print(node.left)
+                        
+                print(node.value)
+                    
+            if node.right is not None:
+                node.in_order_print(node.right)
+
+            
+            
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
-
+            # create a new queue
+            queue = deque()
+            # add the current node to the queue
+            queue.append(node)
+            # run if the queue is not empty
+            while len(queue) > 0:
+                current = queue.popleft()
+                print(current.value)
+            # if there is a node to the left of the current node, add to queue
+            if current.left is not None:
+                queue.append(current.left)
+            # if there is a node to the right of the current node, add to the queue
+            if current.right is not None:
+                queue.append(current.right)
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        stack.push(node)
+        while len(stack) > 0:
+            current = stack.pop()
+            print(current.value)
+            if current.left is not None:
+                stack.push(current.left)
+            if current.right is not None:
+                stack.push(current.right)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+            pass
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+            pass
---
+++ binary_search_tree/bst_doubly_linked_list.py
@@ -0,0 +1,140 @@
+"""Each ListNode holds a reference to its previous node
+as well as its next node in the List."""
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    """Wrap the given value in a ListNode and insert it
+    after this node. Note that this node could already
+    have a next node it is point to."""
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
+    accordingly, effectively deleting this ListNode."""
+    def delete(self):
+        if self.prev:
+            # have this node's previous point to this nodes next
+            self.prev.next = self.next
+        if self.next:
+            # have this node's next point to this nodes
+            self.next.prev = self.prev
+
+
+"""Our doubly-linked list class. It holds references to
+the list's head and tail nodes."""
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new head of the list. Don't forget to handle 
+    the old head node's previous pointer accordingly."""
+    def add_to_head(self, value):
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        
+        else:
+            new_node.next = self.head
+            
+            self.head.prev = new_node
+            
+            self.head = new_node
+
+    """Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node."""
+    def remove_from_head(self):
+        value = self.head.value
+        self.delete(self.head)
+        return value
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new tail of the list. Don't forget to handle 
+    the old tail node's next pointer accordingly."""
+    def add_to_tail(self, value):
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    """Removes the List's current tail node, making the 
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node."""
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List."""
+    def move_to_front(self, node):
+        # check to see if node is already the head
+        if node is self.head:
+            return None
+        self.add_to_head(node.value)
+        self.delete(node)
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List."""
+    def move_to_end(self, node):
+        if node is self.tail:
+            return None
+        self.add_to_tail(node.value)
+        self.delete(node)
+
+    """Removes a node from the list and handles cases where
+    the node was the head or the tail"""
+    def delete(self, node):
+        if not self.head and not self.tail:
+            return None
+        self.length -= 1
+        #check if head and tail are the same, if so only one node
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        # check if the node is the head, move the head reference
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        # otherwise there's nothing to update
+        else: node.delete()
+            
+        
+    """Returns the highest value currently in the list"""
+    def get_max(self):
+        max = self.head.value
+        current = self.head
+        while current is not None:
+            if current.value > max:
+                max = current.value
+            else:
+                current = current.next
+        return max
